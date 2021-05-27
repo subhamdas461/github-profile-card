00398463e7274936e2bab3e709cc2c7949506961
Subham Das <subhamdas461@gmail.com>
2021-05-27 22:24:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Main from './Components/Main'
+import  './index.css'
+// import Users from './UserAPI'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            res : [],
+            isLoading: true
+        }
+       
+    }
+    componentDidMount(){
+        this.getData();  
+    }
+    getData = () =>{
+        // this.setState({
+        //     isLoading : false,
+        //     res : Users,
+           
+        // })
+        fetch("https://api.github.com/users")
+        .then(response => response.json())
+        .then(data=>this.setState({
+            res : data,
+            isLoading : false
+        })) 
+
+
+      
+    }
+    render() { 
+        const {res} = this.state
+        console.log(res.length,res)
+        let limit = res.message ? <> {res.message} </> : "";
+        if(limit !== ""){
+            return (<p style = {{color:"#fff"}}>{limit}</p>)
+        }
+        var name = res.map(e=>{
+          
+            var show= (this.state.isLoading) ? <h2>Loading...</h2> :
+                   <Main 
+                        key={e.id} 
+                        name={e.login} 
+                        src={e.avatar_url}
+                        url={e.html_url}
+                        apiUrl={e.url}
+                   />
+            return show
+        })     
+        return ( 
+            <>
+               {name}
+            </>
+         );
+    }
 }
-
+ 
 export default App;
